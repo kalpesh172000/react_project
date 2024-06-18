5909430135b0bb4954d0787e4d7e1c8149035bfd
kalpesh <kalpesh200071@gmail.com>
2024-06-18 18:56:20 +0530

    created simple list with react app

---
+++ src/components/simplelist.jsx
@@ -0,0 +1,16 @@
+function Simplelist()
+{
+    return(
+        <>
+        <h1>This Is My Shopping List</h1>
+        <ul>
+            <li>tomato</li>
+            <li>potato</li>
+            <li>chicken</li>
+            <li>chilli</li>
+        </ul>
+        </>
+    )
+}
+
+export default Simplelist 
